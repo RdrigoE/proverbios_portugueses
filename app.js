4af0c838ffa66d219e4eec216f87488dca2361d7
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2024-08-18 00:04:34 +0100

    - update formatting

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-
 function get_random_range(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-async function get_proverbios () {
-    let response = await fetch('./proverbios.json')
-    let data = await response.json()
-    let proverbios = data
-    let proverbios_array = []
-    proverbios.forEach(element => {
-      proverbios_array.push(element.proverbio)
-    });
-    return proverbios_array
+async function get_proverbios() {
+  let response = await fetch(`./proverbios.json`);
+  let proverbios = await response.json();
+  return proverbios.map(e => e.proverbio);
 }
+
 proverbios = get_proverbios()
 
-    
+
 let button = document.getElementById('button')
 let quote = document.getElementById('quote')
 
 button.addEventListener('click', () => {
-      let random_quote = proverbios.then(data => quote.innerHTML = data[get_random_range(0, data.length)])
-
-      // console.log(proverbios[0])
+  proverbios.then(
+    data => quote.innerHTML = data[get_random_range(0, data.length)]
+  )
 })
